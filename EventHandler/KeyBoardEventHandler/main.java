56b7af011702a92fd2406a1adccabc79657fafa1
Nayeem Mahmood <com.debugster@gmail.com>
2018-09-11 01:49:31 +0600

    Add files via upload

---
+++ EventHandler/KeyBoardEventHandler/main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class main {
+    public static void main(String[] args) {
+        KeyBoardFrame frame = new KeyBoardFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(900, 500); // sets frame size
+        frame.setVisible(true); // displays frame
+    }
+}
